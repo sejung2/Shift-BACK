2d4df5aea6036c3488aba1ff018e6cb449ce3d26
sejung2 <seingdev@gmail.com>
2026-04-19 14:08:42 +0900

    refactor: remove unused code

--- Shift/src/main/java/com/project/shift/user/repository/UserRepository.java
+++ Shift/src/main/java/com/project/shift/user/repository/UserRepository.java
@@ -10,8 +10,6 @@ import java.util.Optional;
 // UserDAO 인터페이스를 구현하고, 내부적으로 JPA를 사용합니다.
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity save(UserEntity userEntity); //명시적 선언으로 반환타입 명확화(S->UserEntity)
-
     boolean existsByLoginId(String loginId);
 
     boolean existsByPhone(String phone);
@@ -19,6 +17,5 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByLoginId(String loginId); // 로그인 아이디로 특정 사용자 추출
 
     // 사용자 이름과 연락처로 특정 사용자 추출
-    @Query(value = "SELECT u FROM UserEntity u WHERE u.name = ?1 AND u.phone = ?2")
     Optional<UserEntity> findByNameAndPhone(String name, String phone);
 }
